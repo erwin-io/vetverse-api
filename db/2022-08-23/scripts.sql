46f45bfb4c159f2bdd3cb4d439bed5c229363c15
erwin-io <erwinramirez220@gmail.com>
2022-08-23 17:21:37 +0800

    db

---
+++ db/2022-08-23/scripts.sql
@@ -0,0 +1,296 @@
+USE [vetversedb]
+GO
+/****** Object:  Table [dbo].[Clients]    Script Date: 23/08/2022 5:20:34 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Clients](
+	[ClientId] [bigint] IDENTITY(1,1) NOT NULL,
+	[UserId] [bigint] NOT NULL,
+	[FirstName] [nvarchar](250) NOT NULL,
+	[MiddleName] [nvarchar](250) NULL,
+	[LastName] [nvarchar](250) NOT NULL,
+	[Email] [nvarchar](250) NOT NULL,
+	[MobileNumber] [nvarchar](250) NOT NULL,
+	[Address] [nvarchar](max) NOT NULL,
+	[BirthDate] [date] NOT NULL,
+	[Age] [bigint] NOT NULL,
+	[GenderId] [bigint] NOT NULL,
+ CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED 
+(
+	[ClientId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[EntityStatus]    Script Date: 23/08/2022 5:20:34 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[EntityStatus](
+	[EntityStatusId] [bigint] NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+ CONSTRAINT [PK_EntityStatus] PRIMARY KEY CLUSTERED 
+(
+	[EntityStatusId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Gender]    Script Date: 23/08/2022 5:20:34 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Gender](
+	[GenderId] [bigint] NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+ CONSTRAINT [PK_Gender] PRIMARY KEY CLUSTERED 
+(
+	[GenderId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[migrations]    Script Date: 23/08/2022 5:20:34 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[migrations](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[timestamp] [bigint] NOT NULL,
+	[name] [varchar](255) NOT NULL,
+ CONSTRAINT [PK_8c82d7f526340ab734260ea46be] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[PetCategory]    Script Date: 23/08/2022 5:20:34 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[PetCategory](
+	[PetCategoryId] [bigint] IDENTITY(1,1) NOT NULL,
+	[PetTypeId] [bigint] NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[EntityStatusId] [bigint] NOT NULL,
+ CONSTRAINT [PK_PetCategory] PRIMARY KEY CLUSTERED 
+(
+	[PetCategoryId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[PetType]    Script Date: 23/08/2022 5:20:34 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[PetType](
+	[PetTypeId] [bigint] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[EntityStatusId] [bigint] NOT NULL,
+ CONSTRAINT [PK_PetType] PRIMARY KEY CLUSTERED 
+(
+	[PetTypeId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Roles]    Script Date: 23/08/2022 5:20:34 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Roles](
+	[RoleId] [bigint] NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[Access] [text] NULL,
+ CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED 
+(
+	[RoleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Staff]    Script Date: 23/08/2022 5:20:34 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Staff](
+	[Staffid] [bigint] IDENTITY(1,1) NOT NULL,
+	[UserId] [bigint] NOT NULL,
+	[FirstName] [nvarchar](250) NOT NULL,
+	[MiddleName] [nvarchar](250) NULL,
+	[LastName] [nvarchar](250) NOT NULL,
+	[Email] [nvarchar](250) NOT NULL,
+	[MobileNumber] [nvarchar](250) NOT NULL,
+	[Address] [nvarchar](max) NOT NULL,
+	[GenderId] [bigint] NOT NULL,
+ CONSTRAINT [PK_Staff] PRIMARY KEY CLUSTERED 
+(
+	[Staffid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 23/08/2022 5:20:34 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[UserId] [bigint] IDENTITY(1,1) NOT NULL,
+	[UserTypeId] [bigint] NOT NULL,
+	[Username] [nvarchar](250) NOT NULL,
+	[Password] [nvarchar](250) NOT NULL,
+	[currentHashedRefreshToken] [nvarchar](max) NULL,
+	[IsAdminApproved] [bit] NOT NULL,
+	[RoleId] [bigint] NOT NULL,
+	[Enable] [bit] NOT NULL,
+	[EntityStatusId] [bigint] NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[UserType]    Script Date: 23/08/2022 5:20:34 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserType](
+	[UserTypeId] [bigint] NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+ CONSTRAINT [PK_UserType] PRIMARY KEY CLUSTERED 
+(
+	[UserTypeId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+INSERT [dbo].[EntityStatus] ([EntityStatusId], [Name]) VALUES (1, N'Active')
+GO
+INSERT [dbo].[EntityStatus] ([EntityStatusId], [Name]) VALUES (2, N'Deleted')
+GO
+INSERT [dbo].[Gender] ([GenderId], [Name]) VALUES (1, N'Male')
+GO
+INSERT [dbo].[Gender] ([GenderId], [Name]) VALUES (2, N'Female')
+GO
+INSERT [dbo].[Gender] ([GenderId], [Name]) VALUES (3, N'Rather not say')
+GO
+INSERT [dbo].[Roles] ([RoleId], [Name], [Access]) VALUES (1, N'Admin', N'Appointments,Health Records,Users,Roles')
+GO
+INSERT [dbo].[Roles] ([RoleId], [Name], [Access]) VALUES (2, N'Manager', NULL)
+GO
+INSERT [dbo].[Roles] ([RoleId], [Name], [Access]) VALUES (3, N'Veterinarian', NULL)
+GO
+INSERT [dbo].[Roles] ([RoleId], [Name], [Access]) VALUES (4, N'Front desk', N'Health Records,Appointments')
+GO
+INSERT [dbo].[Roles] ([RoleId], [Name], [Access]) VALUES (5, N'Guest', NULL)
+GO
+INSERT [dbo].[UserType] ([UserTypeId], [Name]) VALUES (1, N'Staff')
+GO
+INSERT [dbo].[UserType] ([UserTypeId], [Name]) VALUES (2, N'Client')
+GO
+SET ANSI_PADDING ON
+GO
+/****** Object:  Index [U_Roles]    Script Date: 23/08/2022 5:20:34 pm ******/
+ALTER TABLE [dbo].[Roles] ADD  CONSTRAINT [U_Roles] UNIQUE NONCLUSTERED 
+(
+	[Name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[PetCategory] ADD  CONSTRAINT [DF_PetCategory_EntityStatusId]  DEFAULT ((1)) FOR [EntityStatusId]
+GO
+ALTER TABLE [dbo].[PetType] ADD  CONSTRAINT [DF_PetType_EntityStatusId]  DEFAULT ((1)) FOR [EntityStatusId]
+GO
+ALTER TABLE [dbo].[Users] ADD  CONSTRAINT [DF_Users_IsAdminApproved]  DEFAULT ((0)) FOR [IsAdminApproved]
+GO
+ALTER TABLE [dbo].[Users] ADD  CONSTRAINT [DF_Users_Enable]  DEFAULT ((1)) FOR [Enable]
+GO
+ALTER TABLE [dbo].[Clients]  WITH CHECK ADD  CONSTRAINT [FK_Clients_Gender] FOREIGN KEY([GenderId])
+REFERENCES [dbo].[Gender] ([GenderId])
+GO
+ALTER TABLE [dbo].[Clients] CHECK CONSTRAINT [FK_Clients_Gender]
+GO
+ALTER TABLE [dbo].[Clients]  WITH CHECK ADD  CONSTRAINT [FK_Clients_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([UserId])
+GO
+ALTER TABLE [dbo].[Clients] CHECK CONSTRAINT [FK_Clients_Users]
+GO
+ALTER TABLE [dbo].[PetCategory]  WITH CHECK ADD  CONSTRAINT [FK_PetCategory_PetType] FOREIGN KEY([PetCategoryId])
+REFERENCES [dbo].[PetType] ([PetTypeId])
+GO
+ALTER TABLE [dbo].[PetCategory] CHECK CONSTRAINT [FK_PetCategory_PetType]
+GO
+ALTER TABLE [dbo].[Staff]  WITH CHECK ADD  CONSTRAINT [FK_Staff_Gender] FOREIGN KEY([GenderId])
+REFERENCES [dbo].[Gender] ([GenderId])
+GO
+ALTER TABLE [dbo].[Staff] CHECK CONSTRAINT [FK_Staff_Gender]
+GO
+ALTER TABLE [dbo].[Staff]  WITH CHECK ADD  CONSTRAINT [FK_Staff_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([UserId])
+GO
+ALTER TABLE [dbo].[Staff] CHECK CONSTRAINT [FK_Staff_Users]
+GO
+ALTER TABLE [dbo].[Users]  WITH CHECK ADD  CONSTRAINT [FK_Users_EntityStatus] FOREIGN KEY([EntityStatusId])
+REFERENCES [dbo].[EntityStatus] ([EntityStatusId])
+GO
+ALTER TABLE [dbo].[Users] CHECK CONSTRAINT [FK_Users_EntityStatus]
+GO
+ALTER TABLE [dbo].[Users]  WITH CHECK ADD  CONSTRAINT [FK_Users_Roles] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[Roles] ([RoleId])
+GO
+ALTER TABLE [dbo].[Users] CHECK CONSTRAINT [FK_Users_Roles]
+GO
+ALTER TABLE [dbo].[Users]  WITH CHECK ADD  CONSTRAINT [FK_Users_UserType] FOREIGN KEY([UserTypeId])
+REFERENCES [dbo].[UserType] ([UserTypeId])
+GO
+ALTER TABLE [dbo].[Users] CHECK CONSTRAINT [FK_Users_UserType]
+GO
+/****** Object:  StoredProcedure [dbo].[usp_Reset]    Script Date: 23/08/2022 5:20:34 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- ====================================================================
+-- Created date: Sept 25, 2020
+-- Author: 
+-- ====================================================================
+CREATE PROCEDURE [dbo].[usp_Reset]
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY
+		SET NOCOUNT ON;
+
+		DELETE FROM [dbo].[PetCategory];
+		DBCC CHECKIDENT ('PetCategory', RESEED, 0)
+		DELETE FROM [dbo].[PetType];
+		DBCC CHECKIDENT ('PetType', RESEED, 0)
+		DELETE FROM [dbo].[Clients];
+		DBCC CHECKIDENT ('Clients', RESEED, 0)
+		DELETE FROM [dbo].[Staff];
+		DBCC CHECKIDENT ('Staff', RESEED, 0)
+		DELETE FROM [dbo].[Users];
+		DBCC CHECKIDENT ('Users', RESEED, 0)
+        
+    END TRY
+    BEGIN CATCH
+
+        SELECT
+            'Error'           AS Status,
+            ERROR_NUMBER()    AS ErrorNumber,
+            ERROR_SEVERITY()  AS ErrorSeverity,
+            ERROR_STATE()     AS ErrorState,
+            ERROR_PROCEDURE() AS ErrorProcedure,
+            ERROR_LINE()      AS ErrorLine,
+            ERROR_MESSAGE()   AS ErrorMessage;
+
+    END CATCH
+
+END
+
+
+GO
