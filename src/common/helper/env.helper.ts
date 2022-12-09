e1196387c9f3cca160cec31f73995f8ad8210d81
erwin-io <erwinramirez220@gmail.com>
2022-12-09 22:36:39 +0800

    node envi

--- src/common/helper/env.helper.ts
+++ src/common/helper/env.helper.ts
@@ -3,7 +3,8 @@ import { existsSync } from "fs";
 import { resolve } from "path";
 
 export function getEnvPath(dest: string): string {
-  const env: string | undefined = process.env.NODE_ENV;
+  // const env: string | undefined = process.env.NODE_ENV;
+  const env: string | undefined = process.env["NODE" + "_ENV"];
   const fallback: string = resolve(`${dest}/.env`);
   const filename: string = env ? `${env}.env` : "development.env";
   let filePath: string = resolve(`${dest}/${filename}`);
